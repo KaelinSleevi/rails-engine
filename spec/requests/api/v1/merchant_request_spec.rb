10a848cee32b5559fde978f937d2a0c9430014db
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-17 20:10:55 -0600

    refactor migrations

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 
 describe "Merchants API" do
   it "sends a list of merchants" do
+
+   5.times do 
+    Merchant.create(
+        name: Faker::Name.name,
+    )
+   end
     create_list(:merchant, 5)
 
     get '/api/v1/merchants/search'
 
     expect(response).to be_successful
+
+    books = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(5)
+
+    merchants.each do |book|
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
   end
 end
