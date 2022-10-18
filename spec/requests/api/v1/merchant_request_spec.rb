d643a40ee15070a6daf545536555c1a8e0bfc05e
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-17 21:12:35 -0600

    feat: add work for getting list of merchants, all tests passing

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,20 +1,15 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  xit "sends a list of merchants" do
+  it "sends a list of merchants" do
 
-   5.times do 
-    Merchant.create(
-        name: Faker::Name.name,
-    )
-   end
     create_list(:merchant, 5)
 
     get '/api/v1/merchants/search'
 
     expect(response).to be_successful
 
-    books = JSON.parse(response.body, symbolize_names: true)
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
     expect(merchants.count).to eq(5)
 
