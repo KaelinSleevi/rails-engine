26df9eaf2ddb2004f773ea6a86e07bf12314a8e3
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-18 17:14:13 -0600

    test: add more tests to get all merchants

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -16,6 +16,29 @@ describe "Merchants API" do
     merchants[:data].each do |data|
       expect(data[:attributes]).to have_key(:name)
       expect(data[:attributes][:name]).to be_a(String)
+
+      expect(data).to have_key(:id)
+      expect(data[:id]).to be_a(String)
+
+      expect(data).to have_key(:type)
+      expect(data[:type]).to be_a(String)
+    end
+  end
+
+  xit "can get one merchant by its id" do
+    id = create(:merchant).id
+  
+    get "/api/v1/books/#{id}"
+  
+    merchant = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(response).to be_successful
+
+    merchant[:data].each do |data|
+      expect(data).to have_key(:id)
+      expect(data[:id]).to be_a(String)
+
+
     end
   end
 end
