2d1b33fd7731565880e9911b33870f781e62f0c2
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-19 09:53:38 -0600

    refactor: add format to validations test transaction spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Transaction, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of :invoice_id }
-    it { should validate_numericality_of :invoice_id }
-    it { should validate_presence_of :credit_card_number}
-    it { should validate_numericality_of :credit_card_number }
-    it { should validate_numericality_of :credit_card_expiration_date }
-    it { should validate_presence_of :result}
+    it { should validate_presence_of(:invoice_id) }
+    it { should validate_numericality_of(:invoice_id) }
+    it { should validate_presence_of(:credit_card_number) }
+    it { should validate_numericality_of(:credit_card_number) }
+    it { should validate_numericality_of(:credit_card_expiration_date) }
+    it { should validate_presence_of(:result) }
   end
 end
