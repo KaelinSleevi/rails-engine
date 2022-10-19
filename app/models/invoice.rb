ec826fdf91e629eb049dd61616324783de2944b3
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-19 09:36:32 -0600

    add validations to models with costumer validations test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,8 @@ class Invoice < ApplicationRecord
  has_many :transactions
  has_many :invoice_items
  has_many :items, through: :invoice_items
+
+ validates_presence_of :customer_id
+ validates_presence_of :status
+ validates_numericality_of :customer_id
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,14 @@
 class InvoiceItem < ApplicationRecord
  belongs_to :item
  belongs_to :invoice
+
+ validates_presence_of :item_id
+ validates_numericality_of :item_id
+ validates_presence_of :invoice_id
+ validates_numericality_of :invoice_id
+ validates_presence_of :quantity
+ validates_numericality_of :quantity
+ validates_presence_of :unit_price
+ validates_numericality_of :unit_price
+ validates_presence_of :status
 end
