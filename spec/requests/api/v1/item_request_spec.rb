be1f870cafd79f3e4ac5734bdf0c3817e921124f
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-10-18 20:38:17 -0600

    test: create test for creating an item

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -31,7 +31,7 @@ describe "Items API" do
     item = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-
+   
     expect(item.count).to eq(1)
 
     expect(item[:data]).to have_key(:id)
@@ -40,4 +40,24 @@ describe "Items API" do
     expect(item[:data]).to have_key(:type)
     expect(item[:data][:type]).to be_a(String)
   end
+
+  it "can create a new item" do
+   item_params = ({
+                   name: 'Leaf Earrings',
+                   description: 'its pretty and green',
+                   unit_price: 12496,
+                   merchant_id: 1
+                 })
+   headers = {"CONTENT_TYPE" => "application/json"}
+ 
+   # We include this header to make sure that these params are passed as JSON rather than as plain text
+   post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+   created_item = Item.last
+ 
+   expect(response).to be_successful
+   expect(created_item.name).to eq(item_params[:name])
+   expect(created_item.description).to eq(item_params[:description])
+   expect(created_item.unit_price).to eq(item_params[:unit_price])
+   expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+ end
 end
